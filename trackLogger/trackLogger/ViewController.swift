c6f046ee8a59683f06c8d3c0c2d9d56507905386
inakidml <inaki.diaz@ikasle.egibide.org>
2017-02-05 17:58:46 +0100

    proyecto creado y cocoa de gmaps

---
+++ trackLogger/trackLogger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trackLogger
+//
+//  Created by Iñaki Diaz on 5/2/17.
+//  Copyright © 2017 Iñaki Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
