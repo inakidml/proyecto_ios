511464d6ebc1f62411489a9f32502c2924053c7a
Iñaki <8fprogmm02@a224p02.arriagainfo.local>
2017-02-07 20:38:25 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 02cbddef6bcddcc068bc18381182acf562e814ab, reversing
    changes made to 5f3f048382d796fb3dd4e0ff0d1d9cf1eb45b7fd.

--- trackLogger/trackLogger/TrackViewController.swift
+++ trackLogger/trackLogger/TrackViewController.swift
@@ -10,52 +10,16 @@ import UIKit
 
 class TrackViewController: UIViewController {
 
-    var counter = 0
-    var timer = Timer()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-<<<<<<< HEAD
     
     
         let mapa = self.childViewControllers[0] as? MapaViewController
         print(mapa?.prueba)
-=======
-        
     }
 
-    @IBOutlet weak var LabelTiempo: UILabel!
-    
-    
-    @IBAction func StartButton(_ sender: UIButton) {
-        timer.invalidate() // Por si se pulsa multiples veces
-        
-        // Arrancar Contador
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
-    }
-    
-    
-    @IBAction func StopButton(_ sender: UIButton) {
-        timer.invalidate()
-        
-       // var counterSalvado = counter  //Por Si se usa mas tarde para guardar los tiempos
-        counter = 0
-    }
-    
-    
-    @IBAction func PauseButton(_ sender: UIButton) {
-        timer.invalidate()
-    }
-    
-    func timerAction() {
-        counter += 1
-        LabelTiempo.text = "\(counter)s"
->>>>>>> origin/master
-    }
-
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
